d9407508651fc305f5194b9ae279bb798faf3dd1
YuqingXiong <2041306083@qq.com>
2024-01-13 23:18:39 +0800

    统一异常处理：异常封装，拦截，返回

--- springmvc_04_ssm/src/main/java/com/rainsun/service/Impl/BookServiceImpl.java
+++ springmvc_04_ssm/src/main/java/com/rainsun/service/Impl/BookServiceImpl.java
@@ -1,7 +1,10 @@
 package com.rainsun.service.Impl;
 
+import com.rainsun.controller.Code;
 import com.rainsun.dao.BookDao;
 import com.rainsun.domain.Book;
+import com.rainsun.exception.BusinessException;
+import com.rainsun.exception.SystemException;
 import com.rainsun.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -33,6 +36,16 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Book getById(Integer id) {
+        if(id == 1){
+            throw new BusinessException(Code.BUSINESS_ERR, "输入参数错误");
+        }
+        // 将可能出现的异常进行包装，转换为自定义异常
+        // 一般不会下面这样写，只是为了包装异常为自己的异常
+        try {
+            int i = 1/0;
+        } catch (Exception e) {
+            throw new SystemException(Code.SYSTEM_ERR, "服务器错误", e);
+        }
         return bookDao.getById(id);
     }
 
